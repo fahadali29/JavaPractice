e8d752c1b0528d3492212c0c31f0489d24d0fb7b
Fahad <fahadali2973747@gmail.com>
2023-12-15 11:33:58 +0500

    textview

--- app/src/main/java/com/example/javapractice/MainActivity.java
+++ app/src/main/java/com/example/javapractice/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.javapractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
